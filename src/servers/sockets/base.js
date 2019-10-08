8910e8b2abbc16bfd4b15bc7bfad6ec09a23666b
Pramod <pramod.bhakta@leadschool.in>
2019-10-09 03:35:38 +0530

    profanity

--- src/servers/sockets/base.js
+++ src/servers/sockets/base.js
@@ -1,10 +1,13 @@
 let messageServices = require('../services/messages.services');
+var profanity = require("profanity-hindi");
+
 module.exports = function (io) {
     io.on('connection', function (socket) {
         socket.on('user:logout', function (data) {
             io.sockets.emit('user:logout', data)
         });
         socket.on('chat', function (data) {
+            data.message = profanity.maskBadWords(data.message);
             io.sockets.emit('chat', data);
             messageServices.addMessageToDataStore(data);
         });

a663c8704f8e035ee8069da8534a41df200828b5
Pramod <pramod.bhakta@leadschool.in>
2019-10-08 17:42:05 +0530

    initial packages knex and server code

--- src/servers/app.js
+++ src/servers/app.js
@@ -1,14 +1,24 @@
-const http = require('http');
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+let dotenv = require('dotenv');
+global.db = require('./db/knexfile');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-const hostname = '127.0.0.1';
-const port = process.env.PORT || 8080;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+io.on('connection', function (socket) {
+    console.log('a user connected');
 });
+
+http.listen(process.env.PORT || 8080, function () {
+    console.log('listening on *:8080');
+});
+
+app.use(bodyParser.json({ limit: '5mb' }));
+app.use(bodyParser.urlencoded({ limit: '5mb', extended: false }));
+app.use(cookieParser());

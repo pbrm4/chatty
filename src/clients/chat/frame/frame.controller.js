f7f927fc2a11fa302caf4c3b61f089ad7d41074c
Ankush Jadhav <ankush@leadschool.in>
2019-10-09 03:29:59 +0530

    fix

--- src/clients/chat/frame/frame.controller.js
+++ src/clients/chat/frame/frame.controller.js
@@ -5,11 +5,11 @@ angular.module('chat.frame')
     .component('chatFrame', {
         templateUrl: 'chat/frame/frame.template.html',
         controller: ['$cookies', '$location', function FrameController($cookies, $location) {
-            this.name = JSON.parse(sessionStorage.user).Name;
+            this.session = JSON.parse(sessionStorage.user);
 
             this.logoutService = function () {
                 $cookies.remove('jwt');
-                socket.emit('user:logout', { name: this.name });
+                socket.emit('user:logout', { name: this.session.name, email: this.session.email_id });
                 sessionStorage.removeItem('user')
                 $location.url('/login');
             };

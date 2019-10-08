2c5a497342da9bcf55dea0aab8efa049da81b866
Ankush Jadhav <ankush@leadschool.in>
2019-10-09 03:48:45 +0530

    commit

--- src/clients/app.config.js
+++ src/clients/app.config.js
@@ -2,6 +2,11 @@
 
 angular.module('chattyApp')
     .config(['$routeProvider', function config($routeProvider) {
+        var $cookies;
+        angular.injector(['ngCookies']).invoke(['$cookies', function (_$cookies_) {
+            $cookies = _$cookies_;
+        }]);
+
         $routeProvider
             .when('/signup', {
                 template: '<sign-up></sign-up>'
@@ -10,7 +15,11 @@ angular.module('chattyApp')
                 template: '<log-in></log-in>'
             })
             .when('/chat', {
-                template: '<chat></chat>'
+                template: '<chat></chat>',
+                redirectTo: function() {
+                    if (!$cookies.get('jwt'))
+                        return '/login'
+                }
             })
             .otherwise('/login');
     }]);

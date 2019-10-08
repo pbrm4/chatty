8b8da1d0a6c235cd33330183ce04ac4b77b0ca3d
Pramod <pramod.bhakta@leadschool.in>
2019-10-09 02:39:22 +0530

    logout service added

--- src/clients/chat/frame/frame.controller.js
+++ src/clients/chat/frame/frame.controller.js
@@ -1,9 +1,18 @@
 'use strict';
 
+
 angular.module('chat.frame')
     .component('chatFrame', {
         templateUrl: 'chat/frame/frame.template.html',
-        controller: [function FrameController() {
+        controller: ['$cookies', '$location', function FrameController($cookies, $location) {
             this.name = JSON.parse(sessionStorage.user).Name;
+
+            this.logoutService = function () {
+                $cookies.remove('jwt');
+                socket.emit('user:logout', { name: this.name });
+                sessionStorage.removeItem('user')
+                $location.url('/login');
+            };
+
         }]
     });

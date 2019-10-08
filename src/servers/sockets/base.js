c647e84088bc76536cf475839eecb9e87c0845c9
Pramod <pramod.bhakta@leadschool.in>
2019-10-09 03:29:12 +0530

    logout pe changed

--- src/servers/sockets/base.js
+++ src/servers/sockets/base.js
@@ -2,7 +2,7 @@ let messageServices = require('../services/messages.services');
 module.exports = function (io) {
     io.on('connection', function (socket) {
         socket.on('user:logout', function (data) {
-            io.sockets.emit('user:logout', { name: data.name })
+            io.sockets.emit('user:logout', data)
         });
         socket.on('chat', function (data) {
             io.sockets.emit('chat', data);

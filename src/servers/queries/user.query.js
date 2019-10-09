fa1a2dc1db967584303975b5b1c3bf3bdeb3671f
Pramod <pramod.bhakta@leadschool.in>
2019-10-09 12:16:27 +0530

    signup nikala

--- src/servers/queries/user.query.js
+++ src/servers/queries/user.query.js
@@ -1,9 +1,3 @@
-exports.addNewUser = function (user) {
-    return db.from('users')
-        .insert(user)
-        .returning('*');
-}
-
 exports.getUserForLogin = function (email_id) {
     return db.from('users')
         .where('email_id', email_id)
--- src/servers/routers/auth.router.js
+++ src/servers/routers/auth.router.js
@@ -1,10 +1,8 @@
 let express = require('express');
 let router = express.Router();
 
-let signupCtrl = require('../controllers/signup.ctrl');
 let loginCtrl = require('../controllers/login.ctrl');
 
-router.post('/signup', signupCtrl.userSignup);
 router.post('/login', loginCtrl.login);
 
 module.exports = router;
